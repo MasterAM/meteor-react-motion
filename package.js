37ffa733e24a2c2d1d4a2dc6b5e6b62f73d0dcaa
Alon Amster <m@ster.am>
2015-11-22 13:16:03 +0200

    Updaed to 0.3.1.
    Using React v0.1.4 and react-motion v0.3.1.
    The package now tracks the react-motion version.

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'azfx:react-motion',
-  version: '0.1.1',
+  version: '0.3.1',
   summary: 'React-Motion Animation Library packaged for Meteor',
   git: 'https://github.com/azfx/meteor-react-motion',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  'react-motion' : '0.2.7',
+  'react-motion' : '0.3.1',
   'externalify': '0.1.0'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2');
   api.use('ecmascript');
-  api.use(['react@0.1.13', 'cosmos:browserify@0.5.1']);
-  api.imply(['react@0.1.13']);
+  api.use(['react@0.14.1_1', 'cosmos:browserify@0.5.1']);
+  api.imply(['react@0.14.1_1']);
   api.addFiles('package.browserify.js');
   api.addFiles('package.browserify.options.json');
 
